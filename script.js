be879cf4fdcc07d18d324fc4403a5df34b0d228c
armeeng <138351641+armeeng@users.noreply.github.com>
2023-07-07 02:46:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.navbar__menu');
+    
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    });
 
-window.addEventListener('DOMContentLoaded', function() {
+    const aboutButton = document.querySelector('.navbar__links[href="#about"]');
+    
+    aboutButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      const aboutSection = document.getElementById('about');
+      const offset = 100; // Adjust this value to set the desired offset
+      const top = aboutSection.getBoundingClientRect().top + window.pageYOffset - offset;
+      window.scrollTo({ top, behavior: 'smooth' });
+    });
+
+    const learnButton = document.querySelector('.main__btn');
+
+    learnButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      const aboutSection = document.getElementById('about');
+      const offset = 100; // Adjust this value to set the desired offset
+      const top = aboutSection.getBoundingClientRect().top + window.pageYOffset - offset;
+      window.scrollTo({ top, behavior: 'smooth' });
+    });
+    
+
+    const educationButton = document.querySelector('.navbar__links[href="#education"]');
+    
+    educationButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      const educationSection = document.getElementById('education');
+      const offset = 100; // Adjust this value to set the desired offset
+      const top = educationSection.getBoundingClientRect().top + window.pageYOffset - offset;
+      window.scrollTo({ top, behavior: 'smooth' });
+    });
+
     var skillsBar = document.querySelector('.SkillsBar');
     skillsBar.scrollLeft = (skillsBar.scrollWidth - skillsBar.clientWidth) / 2;
-  });
+
+    const homeButton = document.querySelector('.navbar__links[href="#home"]');
+
+    homeButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        window.scrollTo({top: 0, behavior: 'smooth'})
+    });
+});
+
+  
+ 
+
+  
+  
+  
