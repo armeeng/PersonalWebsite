8c7f28e7fa932a904267da48cd0981f7ece271e3
armeeng <138351641+armeeng@users.noreply.github.com>
2023-07-12 15:31:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,50 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-    const menu = document.querySelector('#mobile-menu');
-    const menuLinks = document.querySelector('.navbar__menu');
-    
-    menu.addEventListener('click', function() {
-        menu.classList.toggle('is-active');
-        menuLinks.classList.toggle('active');
-    });
+  const menu = document.querySelector('#mobile-menu');
+  const menuLinks = document.querySelector('.navbar__menu');
 
-    const aboutButton = document.querySelector('.navbar__links[href="#about"]');
-    
-    aboutButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      const aboutSection = document.getElementById('about');
-      const offset = 100; // Adjust this value to set the desired offset
-      const top = aboutSection.getBoundingClientRect().top + window.pageYOffset - offset;
-      window.scrollTo({ top, behavior: 'smooth' });
-    });
+  menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+  });
 
-    const learnButton = document.querySelector('.main__btn');
+  const aboutButton = document.querySelector('.navbar__links[href="#about"]');
 
-    learnButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      const aboutSection = document.getElementById('about');
-      const offset = 100; // Adjust this value to set the desired offset
-      const top = aboutSection.getBoundingClientRect().top + window.pageYOffset - offset;
-      window.scrollTo({ top, behavior: 'smooth' });
-    });
-    
+  aboutButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const aboutSection = document.getElementById('about');
+    const offset = 100; // Adjust this value to set the desired offset
+    const top = aboutSection.getBoundingClientRect().top + window.pageYOffset - offset;
+    window.scrollTo({ top, behavior: 'smooth' });
+  });
 
-    const educationButton = document.querySelector('.navbar__links[href="#education"]');
-    
-    educationButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      const educationSection = document.getElementById('education');
-      const offset = 100; // Adjust this value to set the desired offset
-      const top = educationSection.getBoundingClientRect().top + window.pageYOffset - offset;
-      window.scrollTo({ top, behavior: 'smooth' });
-    });
+  const learnButton = document.querySelector('.main__btn');
 
-    var skillsBar = document.querySelector('.SkillsBar');
-    skillsBar.scrollLeft = (skillsBar.scrollWidth - skillsBar.clientWidth) / 2;
+  learnButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const aboutSection = document.getElementById('about');
+    const offset = 100; // Adjust this value to set the desired offset
+    const top = aboutSection.getBoundingClientRect().top + window.pageYOffset - offset;
+    window.scrollTo({ top, behavior: 'smooth' });
+  });
 
-    const homeButton = document.querySelector('.navbar__links[href="#home"]');
+  const educationButton = document.querySelector('.navbar__links[href="#education"]');
 
-    homeButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        window.scrollTo({top: 0, behavior: 'smooth'})
-    });
+  educationButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const educationSection = document.getElementById('education');
+    const offset = 100; // Adjust this value to set the desired offset
+    const top = educationSection.getBoundingClientRect().top + window.pageYOffset - offset;
+    window.scrollTo({ top, behavior: 'smooth' });
+  });
+
+  var skillsBar = document.querySelector('.SkillsBar');
+  skillsBar.scrollLeft = (skillsBar.scrollWidth - skillsBar.clientWidth) / 2;
+
+  const homeButton = document.querySelector('.navbar__links[href="#home"]');
+
+  homeButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
 });
-
-  
- 
-
-  
-  
-  
